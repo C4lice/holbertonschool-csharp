37fcdefe8718548bd4e762524839b284b0289c58
C4lice <laura.aueptit002@gmail.com>
2025-12-03 13:46:52 +0100

    sends empty files

---
+++ csharp-hello_world/5-print_float/5-print_float.cs
@@ -0,0 +1,10 @@
+using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float number = 3.14159F;
+        Console.WriteLine("Float: {0}", number);
+        }
+    }
