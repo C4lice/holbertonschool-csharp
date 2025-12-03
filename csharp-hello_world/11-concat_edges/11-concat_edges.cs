2d916009a09b255fea1cd8f6a310152ec3ff206c
C4lice <laura.aueptit002@gmail.com>
2025-12-03 19:11:23 +0100

    Correction of substring concatenation in 11-concat_edges.cs

--- csharp-hello_world/11-concat_edges/11-concat_edges.cs
+++ csharp-hello_world/11-concat_edges/11-concat_edges.cs
@@ -5,6 +5,6 @@ class Program
     static void Main(string[] args)
     {
         string str = "C# (pronounced \"See Sharp\") is a simple, modern, object-oriented, and type-safe programming language. C# has its roots in the C family of languages and will be immediately familiar to C, C++, Java, and JavaScript programmers.";
-        Console.WriteLine(str.Substring(49, 15) + " " + str.Substring(80, 11) + str.Substring(118, 4) + str.Substring(102, 2));
+        Console.WriteLine(str.Substring(49, 15) + str.Substring(64, 1) + str.Substring(80, 11) + str.Substring(118, 4) + str.Substring(102, 2));
     }
 }
