7b0ab9b24bccf13d032710830930a6e29a76aed1
C4lice <laura.aupetit002@gmail.com>
2025-12-04 23:18:57 +0100

    Added programme to print the alphabet in lowercase letters and created the associated project file.

---
+++ csharp-ifelse_loops_methods/2-print_alphabet/2-print_alphabet.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (char letter = 'a'; letter <= 'z'; letter++)
+        {
+            Console.Write(letter);
+        }
+        Console.WriteLine();
+    }
+}
