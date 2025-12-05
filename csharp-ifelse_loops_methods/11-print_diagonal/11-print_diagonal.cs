f88f284e1b74f94292c84f04c33108b513a009a8
C4lice <laura.aupetit002@gmail.com>
2025-12-05 17:37:28 +0100

    change of the variable for additional space

--- csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
+++ csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -1,21 +1,20 @@
 using System;
+
 class Line
 {
     public static void PrintDiagonal(int length)
     {
-        if (length <= 0)
+        if (length > 0)
         {
-            Console.WriteLine();
-            return;
-        }
-
-        for (int i = 0; i < length; i++)
-        {
-            for (int j = 0; j < i; j++)
+            for (int i = 0 ; i < length ; i++)
             {
-                Console.Write(' ');
+                for (int Y = 0 ; Y < i ; Y++)
+                {
+                    Console.Write(" ");
+                }
+                Console.WriteLine("\\"); 
             }
-            Console.WriteLine('\\');
         }
+        Console.WriteLine("");
     }
 }
