813db545a0937305a5f1aff2e404971ebf8093af
C4lice <laura.aupetit002@gmail.com>
2025-12-05 19:19:23 +0100

    fix: missing variable

--- csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
+++ csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -4,13 +4,17 @@ class Array
 {
     public static void Reverse(int[] array)
     {
-        for (int i = array.Length - 1; i >= 0; i--)
+        if (array != null && array.Length >= 1)
         {
-            Console.Write(array[i]);
-            if (i != 0)
+            for (int i = array.Length - 1 ; i >= 0 ; i--)
             {
-                Console.Write(" ");
-            }   
+                Console.Write($"{array[i]}"); 
+                if (i > 0)
+                {
+                    Console.Write(" ");
+                }
+            }
         }
+        Console.WriteLine("");
     }
 }
