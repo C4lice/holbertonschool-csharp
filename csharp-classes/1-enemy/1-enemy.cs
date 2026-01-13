1fa30b803a0dde7df89df66f546192c2bc20ca30
C4lice <laura.aupetit002@gmail.com>
2026-01-13 14:34:32 +0100

    Added the Zombie class and associated project file

---
+++ csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// This is a class that represent zombie enemy.
+    /// </summary>
+    public class Zombie
+    {
+        /// <summary>
+        /// Represents the zombie's health.
+        /// </summary>
+        public int health;
+
+        public Zombie()
+        {
+            health = 0;
+        }
+    }
+}
