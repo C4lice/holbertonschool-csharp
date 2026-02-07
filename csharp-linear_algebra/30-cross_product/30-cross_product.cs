3a50b370a3912028df8e9df549d127ce028802dd
C4lice <laura.aupetit002@gmail.com>
2026-02-07 01:38:05 +0100

    Refactor CrossProduct method to ensure 3D vector validation and correct calculation

--- csharp-linear_algebra/30-cross_product/30-cross_product.cs
+++ csharp-linear_algebra/30-cross_product/30-cross_product.cs
@@ -1,19 +1,26 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 
-class MatrixMath
+public class VectorMath
 {
     public static double[] CrossProduct(double[] vector1, double[] vector2)
     {
-        if (vector1.Count() != 3 || vector2.Count() != 3)
+        // Vérification que les deux vecteurs sont des vecteurs 3D
+        if (vector1 == null || vector2 == null ||
+            vector1.Length != 3 || vector2.Length != 3)
         {
-            return new double[]{-1};
+            return new double[] { -1 };
         }
-        return new double[] {
-            vector1[2]*vector2[3] - vector1[3]*vector2[2],
-            vector1[3]*vector2[1] - vector1[1]*vector2[3],
-            vector1[1]*vector2[2] - vector1[2]*vector2[1]
-            };
+
+        // Calcul du produit vectoriel
+        double[] result = new double[3];
+
+        result[0] = vector1[1] * vector2[2] - vector1[2] * vector2[1]; // x
+        result[1] = vector1[2] * vector2[0] - vector1[0] * vector2[2]; // y
+        result[2] = vector1[0] * vector2[1] - vector1[1] * vector2[0]; // z
+
+        return result;
     }
 }
