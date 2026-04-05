123c285a24a5e9b3ba58012200ff35bd4853e6ee
C4lice <laura.aupetit002@gmail.com>
2026-04-05 14:05:17 +0200

    Added the `Player` class with health management and the associated project file.

---
+++ csharp-delegates_events/2-validation/2-validation.cs
@@ -0,0 +1,71 @@
+using System;
+
+public class Player
+{
+    // Delegate
+    public delegate void CalculateHealth(float amount);
+
+    // Propriétés privées
+    private string name;
+    private float maxHp;
+    private float hp;
+
+    // Constructeur
+    public Player(string name = "Player", float maxHp = 100f)
+    {
+        this.name = name;
+
+        if (maxHp <= 0)
+        {
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+            this.maxHp = 100f;
+        }
+        else
+        {
+            this.maxHp = maxHp;
+        }
+
+        this.hp = this.maxHp;
+    }
+
+    // Affichage
+    public void PrintHealth()
+    {
+        Console.WriteLine($"{name} has {hp} / {maxHp} health");
+    }
+
+    // TakeDamage
+    public void TakeDamage(float damage)
+    {
+        if (damage < 0)
+            damage = 0;
+
+        Console.WriteLine($"{name} takes {damage} damage!");
+
+        float newHp = hp - damage;   // 🔹 calcul uniquement
+        ValidateHP(newHp);           // 🔹 validation
+    }
+
+    // HealDamage
+    public void HealDamage(float heal)
+    {
+        if (heal < 0)
+            heal = 0;
+
+        Console.WriteLine($"{name} heals {heal} HP!");
+
+        float newHp = hp + heal;     // 🔹 calcul uniquement
+        ValidateHP(newHp);           // 🔹 validation
+    }
+
+    // Validation des HP
+    public void ValidateHP(float newHp)
+    {
+        if (newHp < 0)
+            hp = 0;
+        else if (newHp > maxHp)
+            hp = maxHp;
+        else
+            hp = newHp;
+    }
+}
